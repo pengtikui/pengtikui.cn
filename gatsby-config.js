b00cf6b3bf1ec4b151ceb5b41a5ac133c680aebf
pengtikui <949828390@qq.com>
2019-09-27 15:54:08 +0800

    Update Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-74390371-1`,
       },
     },
     `gatsby-plugin-feed`,
