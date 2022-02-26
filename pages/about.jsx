68fa836846ab03cab7f81c948d55ec9a54fd286d
pengtikui <949828390@qq.com>
2022-02-27 01:20:13 +0800

    chore: upgrade deps

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,4 +1,3 @@
-import { DiscussionEmbed } from 'disqus-react';
 import PageHead from '../shared/PageHead';
 import AuthorHeader from '../shared/AuthorHeader';
 
@@ -10,13 +9,6 @@ export default function About() {
       <div className="prose dark:prose-light max-w-none mt-10 mb-8">
         <p>Paranoid_K 的个人站点。</p>
       </div>
-      <DiscussionEmbed
-        shortname="pengtikui"
-        config={{
-          title: '关于',
-          language: 'zh',
-        }}
-      />
     </>
   );
 }
