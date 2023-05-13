aa19e2ff751bcf4b731a27ffe679e2f1b381b04e
pengtikui <949828390@qq.com>
2023-05-13 13:43:06 +0800

    feat: 优化列表样式

--- components/List.tsx
+++ components/List.tsx
@@ -14,9 +14,12 @@ const List: FC<ListProps> = ({ data, className }) => {
           key={item.slug}
           className="px-3 py-2 mt-1 rounded-md transition-colors hover:bg-gray-100"
         >
-          <Link href={`/${item.type}/${item.slug}`} className="flex items-center justify-between">
-            <span className="flex-grow text-gray-900">{item.title}</span>
-            <span className="text-sm text-gray-400">{item.date}</span>
+          <Link
+            href={`/${item.type}/${item.slug}`}
+            className="flex items-center justify-between space-x-2"
+          >
+            <span className="flex-grow text-gray-900 line-clamp-1">{item.title}</span>
+            <span className="flex-shrink-0 text-sm text-gray-400">{item.date}</span>
           </Link>
         </li>
       ))}
